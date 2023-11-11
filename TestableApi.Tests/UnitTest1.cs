b20e5dcff331fd62b08b4704629325893c65fc90
TristanRib <tristanrib_@outlook.fr>
2023-11-11 15:31:22 +0100

    Fix(text): description des scénarios corrigée

--- TestableApi.Tests/UnitTest1.cs
+++ TestableApi.Tests/UnitTest1.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Mvc.Testing;
 public class UnitTest1
 {
     //Scenario : Je demande un fuseau horaire avec un mauvais input
+    /*
+    Scenario : Je récupère le fuseau horaire d'un pays inexistant
+    Given - le pays que je demande est '  '
+    When - demande le fuseau avec un mauvais input
+    Then - 400 - Pays introuvable
+    */
     [Fact]
     public async Task GetTimezoneBadRequest()
     {
@@ -19,14 +25,19 @@ public class UnitTest1
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
-    //Scenario : Je récupère le fuseau horaire d'un pays inexistant
+    /*
+    Scenario : Je récupère le fuseau horaire d'un pays inexistant
+    Given - le pays que je demande est 'test'
+    When - demande le fuseau avec un pays inexistant
+    Then - 404/400 - Ce pays n'est pas connu
+    */
     [Fact]
     public async Task GetUnknownCountry()
     {
         await using var _factory = new WebApplicationFactory<Program>();
         var client = _factory.CreateClient();
 
-        var response = await client.GetAsync("TimeZone/GetTimeZone/_");
+        var response = await client.GetAsync("TimeZone/GetTimeZone/test");
         string stringResponse = await response.Content.ReadAsStringAsync();
         
         //Assert
@@ -34,7 +45,12 @@ public class UnitTest1
         Assert.Equal("Ce pays n'est pas connu.", stringResponse);
     }
 
-    //Scenario : Je demande un pays qui n'a pas son fuseau horaire renseigné
+    /*
+    Scenario : Je récupère le fuseau horaire d'un pays qui n'a pas son fuseau horaire
+    Given - le pays que je demande est 'Belgique'
+    When - demande le fuseau avec un pays sans fuseau horaire
+    Then - 404 - Aucune timezone renseignée pour ce pays
+    */
     [Fact]
     public async Task GetEmptyTimeZone()
     {
@@ -49,7 +65,12 @@ public class UnitTest1
         Assert.Equal("Aucune timezone renseignée pour ce pays.", stringResponse);
     }
 
-    //Scenario : Je récupère le fuseau horaire d'un pays qui existe
+    /*
+    Scenario : Je récupère le fuseau horaire d'un pays qui possède son fuseau horaire renseigné
+    Given - le pays que je demande est 'France'
+    When - demande le fuseau avec un pays renseigné
+    Then - 202 - Timezone
+    */
     [Fact]
     public async Task GetExistingTimeZone()
     {
